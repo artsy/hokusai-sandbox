388fc454f6a717f696987b94900efc4ce026b6eb
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-09-02 16:29:10 +0200

    add /ping endpoint

--- hello.go
+++ hello.go
@@ -11,12 +11,17 @@ func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, world!")
 }
 
+func ping (w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "PONG")
+}
+
 func main() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/ping", ping)
 	port := os.Getenv("PORT")
 	if len(port) == 0 {
 		port = "8080"
 	}
-	fmt.Fprintf(os.Stderr, fmt.Sprintf("Sup, listening on port %s...\n", port))
+	fmt.Fprintf(os.Stderr, fmt.Sprintf("Listening on port %s...\n", port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf("0.0.0.0:%s", port), nil))
 }
