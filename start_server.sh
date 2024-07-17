d153650b2d076675268360eded802528e2694ee3
Jian <jian-feng@artsymail.com>
2024-07-17 09:01:23 -0700

    feat: consider when secrets file does not exist, such as in local docker dev

--- start_server.sh
+++ start_server.sh
@@ -1,3 +1,12 @@
-#!/usr/bin/env sh
-export $(cat /secrets/secrets | xargs)
+#!/bin/sh
+
+# in prod, this file should contain app secret KEY=VALUE configs
+# if file exists, populate env with the vars
+SECRETS_FILE=/secrets/secrets
+if [ -f "$SECRETS_FILE" ]
+then
+  export $(cat "$SECRETS_FILE" | xargs)
+fi
+
+# start app as you normally would
 ./server
